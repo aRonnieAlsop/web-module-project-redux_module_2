8d24ac6ab756cba1c37e535e9f2fa72c9db4d3af
Arron Alsop <alsop.arron@gmail.com>
2023-10-14 18:39:05 -0700

    'wip'

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,4 +2,9 @@ import { combineReducers } from 'redux';
 
 import movieReducer from './movieReducer';
 
+const rootReducer = combineReducers({
+    movieReducer,
+    favoritesReducer
+})
+
 export default movieReducer;
